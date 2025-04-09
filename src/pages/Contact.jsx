8bfcf9a9858d62a7d1b819debc54676182d7b26f
Mohsen-web-coding <mohsenelrhazi3@gmail.com>
2025-04-09 17:17:38 +0100

    modifier desgin de form contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -23,130 +23,127 @@ export default function Contact() {
   return (
     // <div className="flex  shadow-2xl mt-20 bg-white">
       <div className="container mx-auto">
-        <div className="max-w-2xl mx-auto my-10 bg-white p-4 rounded-md shadow-xl">
-          <div className="text-center">
-            <h1 className="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200">
-              Contact Us
-            </h1>
-            <p className="text-gray-400 dark:text-gray-400">
-              Fill up the form below to send us a message.
-            </p>
-          </div>
-          <div className="m-7">
-            <form onSubmit={handleSubmit} className="needs-validation">
-              <div className="flex mb-6 space-x-4">
-                <div className="w-full md:w-1/2">
-                  <label
-                    htmlFor="firstName"
-                    className="block mb-2 text-sm text-gray-600 dark:text-gray-400"
-                  >
-                    First Name
-                  </label>
-                  <input
-                    type="text"
-                    name="firstName"
-                    id="firstName"
-                    placeholder="John"
-                    required
-                    value={formData.firstName}
-                    onChange={handleChange}
-                    className="w-full px-3 py-2 placeholder-gray-300 border-2 border-gray-200 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300"
-                  />
-                </div>
-                <div className="w-full md:w-1/2">
-                  <label
-                    htmlFor="lastName"
-                    className="block mb-2 text-sm text-gray-600 dark:text-gray-400"
-                  >
-                    Last Name
-                  </label>
-                  <input
-                    type="text"
-                    name="lastName"
-                    id="lastName"
-                    placeholder="Doe"
-                    required
-                    value={formData.lastName}
-                    onChange={handleChange}
-                    className="w-full px-3 py-2 placeholder-gray-300 border-2 border-gray-200 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300"
-                  />
-                </div>
-              </div>
+      <div className="max-w-2xl mx-auto my-10 bg-white p-4 rounded-md shadow-xl">
+      <div className="text-center">
+      <h1 className="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200">
+      Contact Us
+      </h1>
+      <p className="text-gray-400 dark:text-gray-400">
+      Fill up the form below to send us a message.
+      </p>
+      </div>
+      <div className="m-7">
+      <form onSubmit={handleSubmit} className="needs-validation">
+      <div className="flex mb-6 space-x-4">
+      <div className="w-full md:w-1/2">
+      <label
+      htmlFor="firstName"
+      className="block mb-2 text-sm text-gray-600 dark:text-gray-400"
+      >
+      First Name
+      </label>
+      <input
+      type="text"
+      name="firstName"
+      id="firstName"
+      placeholder="John"
+      required
+      value={formData.firstName}
+      onChange={handleChange}
+      className="w-full px-3 py-2 placeholder-gray-300 border-2 border-gray-200 rounded-md focus:outline-none  focus:border-[#ff7e00]"
+      />
+      </div>
+      <div className="w-full md:w-1/2">
+      <label
+      htmlFor="lastName"
+      className="block mb-2 text-sm text-gray-600 dark:text-gray-400"
+      >
+      Last Name
+      </label>
+      <input
+      type="text"
+      name="lastName"
+      id="lastName"
+      placeholder="Doe"
+      required
+      value={formData.lastName}
+      onChange={handleChange}
+      className="w-full px-3 py-2 placeholder-gray-300 border-2 border-gray-200 rounded-md focus:outline-none  focus:border-[#ff7e00]"
+      />
+      </div>
+      </div>
 
-              <div className="flex mb-6 space-x-4">
-                <div className="w-full md:w-1/2">
-                  <label
-                    htmlFor="email"
-                    className="block mb-2 text-sm text-gray-600 dark:text-gray-400"
-                  >
-                    Email Address
-                  </label>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    placeholder="you@company.com"
-                    required
-                    value={formData.email}
-                    onChange={handleChange}
-                    className="w-full px-3 py-2 placeholder-gray-300 border-2 border-gray-200 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300"
-                  />
-                </div>
+      <div className="flex mb-6 space-x-4">
+      <div className="w-full md:w-1/2">
+      <label
+      htmlFor="email"
+      className="block mb-2 text-sm text-gray-600 dark:text-gray-400"
+      >
+      Email Address
+      </label>
+      <input
+      type="email"
+      name="email"
+      id="email"
+      placeholder="you@company.com"
+      required
+      value={formData.email}
+      onChange={handleChange}
+      className="w-full px-3 py-2 placeholder-gray-300 border-2 border-gray-200 rounded-md focus:outline-none  focus:border-[#ff7e00]"
+      />
+      </div>
 
-                <div className="w-full md:w-1/2">
-                  <label
-                    htmlFor="phone"
-                    className="block text-sm mb-2 text-gray-600 dark:text-gray-400"
-                  >
-                    Phone Number
-                  </label>
-                  <input
-                    type="text"
-                    name="phone"
-                    id="phone"
-                    placeholder="+1 (555) 1234-567"
-                    required
-                    value={formData.phone}
-                    onChange={handleChange}
-                    className="w-full px-3 py-2 placeholder-gray-300 border-2 border-gray-200 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300"
-                  />
-                </div>
-              </div>
+      <div className="w-full md:w-1/2">
+      <label
+      htmlFor="phone"
+      className="block text-sm mb-2 text-gray-600 dark:text-gray-400"
+      >
+      Phone Number
+      </label>
+      <input
+      type="text"
+      name="phone"
+      id="phone"
+      placeholder="+1 (555) 1234-567"
+      required
+      value={formData.phone}
+      onChange={handleChange}
+      className="w-full px-3 py-2 placeholder-gray-300 border-2 border-gray-200 rounded-md focus:outline-none  focus:border-[#ff7e00]"
+      />
+      </div>
+      </div>
 
-              <div className="mb-6">
-                <label
-                  htmlFor="message"
-                  className="block mb-2 text-sm text-gray-600 dark:text-gray-400"
-                >
-                  Your Message
-                </label>
-                <textarea
-                  rows="5"
-                  name="message"
-                  id="message"
-                  placeholder="Your Message"
-                  required
-                  value={formData.message}
-                  onChange={handleChange}
-                  className="w-full px-3 py-2 placeholder-gray-300 border-2 border-gray-200 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300"
-                ></textarea>
-              </div>
+      <div className="mb-6">
+      <label
+      htmlFor="message"
+      className="block mb-2 text-sm text-gray-600 dark:text-gray-400"
+      >
+      Your Message
+      </label>
+      <textarea
+      rows="5"
+      name="message"
+      id="message"
+      placeholder="Your Message"
+      required
+      value={formData.message}
+      onChange={handleChange}
+      className="w-full px-3 py-2 placeholder-gray-300 border-2 border-gray-200 rounded-md focus:outline-none  focus:border-[#ff7e00]"
+      ></textarea>
+      </div>
 
-              <div className="mb-6">
-                <button
-                  type="submit"
-                  className="w-full px-3 py-3 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none"
-                >
-                  Send Message
-                </button>
-              </div>
-              <p
-                className="text-base text-center text-gray-400"
-                id="result"
-              ></p>
-            </form>
-          </div>
-        </div>
+      <div className="mb-6">
+      <button
+      type="submit"
+      className="w-full px-3 py-3 rounded-md text-white font-semibold bg-[#ff7e00] focus:outline-none focus:ring-2 focus:ring-[#ff7e00] focus:ring-opacity-50 hover:bg-[#ff7e00] active:bg-[#e66e00] transition-transform transform hover:translate-y-[-2px] duration-100 ease-in-out"
+      >
+      Send Message
+      </button>
+      </div>
+      
+      </form>
+      </div>
+      </div>
       </div>
     // </div>
   );
