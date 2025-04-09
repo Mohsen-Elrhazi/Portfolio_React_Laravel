53890bfeb94c6b23651e1e8416201723c9ff3321
Mohsen-web-coding <mohsenelrhazi3@gmail.com>
2025-04-09 13:57:13 +0100

    terminer footer de portfolio

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,29 @@
 import { Link } from 'react-router-dom';
-import Github from '../assets/images/github.png';
-import LinkedIn from '../assets/images/linkedin.png';
-import Facebook from '../assets/images/facebook.png';
+// import Github from '../assets/images/github.png';
+import Github2 from '../assets/images/github2.png';
+import LinkedIn2 from '../assets/images/linkedin2.png';
+import Facebook2 from '../assets/images/facebook2.png';
+
 export default function Footer() {
   return (
-    <footer className="bg-gray-600 text-white py-4 ">
-      <div className="container mx-auto flex items-center justify-around">
-        <p>&copy; 2025 Mon Portfolio. Tous droits réservés.</p>
-        <div className="flex justify-center space-x-4 mt-2">
-          <Link to="#" className="text-gray-400 hover:text-white"><img className="w-8" src={LinkedIn} alt="github"></img></Link>
-          <Link to="#" className="text-gray-400 hover:text-white"><img className="w-8" src={Github} alt="github"></img></Link>
-          <Link to="#" className="text-gray-400 hover:text-white"><img className="w-8" src={Facebook} alt="github"></img></Link>
+    <footer className="border-t-2 w-[calc(100%-10rem)] m-auto py-6  bottom-0 left-0 right-0 bg-white z-0">
+      <div className="container mx-auto flex  items-center justify-evenly">
+        <div className="text-center ">
+          <p className="text-gray-800">Designed & Developed by Mohsen Elrhazi</p>
+        </div>
+        <div className="text-center ">
+          <p className="text-gray-800">&copy; 2025 Mon Portfolio. Tous droits réservés.</p>
+        </div>
+        <div className="flex justify-between items-center space-x-16">
+          <Link to="#" className="hover:scale-110 transition-transform duration-300">
+            <img className="w-7" src={LinkedIn2} alt="linkedin"></img>
+          </Link>
+          <Link to="#" className="hover:scale-110 transition-transform duration-300">
+            <img className="w-7" src={Github2} alt="github"></img>
+          </Link>
+          <Link to="#" className="hover:scale-110 transition-transform duration-300">
+            <img className="w-7" src={Facebook2} alt="facebook"></img>
+          </Link>
         </div>
       </div>
     </footer>
